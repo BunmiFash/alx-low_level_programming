274f4bd5e44efc62e6e6548b8b1efa31583965b4
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-13 08:12:14 +0000

    Error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", err);
+	return (1);
 }
