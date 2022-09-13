abe347fd9bebcc638f97e3b32025aa2472ad5846
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-13 23:24:21 +0000

    Putchar

--- 0x02-funcions_nested_loops/0-putchar.c
+++ 0x02-funcions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry Point
- *Description: Prints _putchar followed by a new line
+ *main - Prints _putchar followed by a new line
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("%s\n", "_putchar");
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
