ddbc696ae689f2640bf1fb06bd8304ff59039cc6
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-14 19:47:11 +0000

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * print_times_table - prints multiplication table up to 15
+ * @n: parameter
+ * Return 0
+ */
+
+void print_times_table(int n)
+{
+	int row;
+	int column;
+	int multiply;
+
+	if (n >= 0 && n <= 15)
+	{
+		row = 0;
+		while (row <= n)
+		{
+			column = 0;
+			while (column <= n)
+			{
+				multiply = row * column;
+				if (column == 0)
+				{
+					_putchar('0' + multiply);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (multiply <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + multiply);
+					}
+					else if (multiply > 9 && multiply < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (multiply / 10));
+						_putchar('0' + (multiply % 10));
+					}
+					else if (multiply >= 100)
+					{
+						_putchar('0' + (multiply / 100));
+						_putchar('0' + ((multiply / 10) % 10));
+						_putchar('0' + (multiply % 10));
+					}
+				}
+				column++;
+			}
+			_putchar('\n');
+			row++;
+		}
+	}
+}
