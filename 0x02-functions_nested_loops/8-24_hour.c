8ff5ff8c3cae4a11de5140c505809731b6153f9e
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-14 10:31:23 +0000

    Jack

---
+++ 0x02-functions_nested_loops/8-24_hour.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hour = 0, min;
+
+	while (hour <= 23)
+	{
+		min = 0;
+		while (min <= 59)
+		{
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		_putchar('\n');
+		hour++;
+	}
+}
