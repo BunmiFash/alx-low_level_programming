a8c3193dee8e3292492a015c2ab34b4715270f0e
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-14 08:37:21 +0000

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: Number whose last digit is printed
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+
+	int last = n % 10;
+
+	_putchar(last);
+	return (0);
+}
