791f3784f7ef86800751c40944c034cb4a37f7fb
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-14 06:31:40 +0000

    Alphabetsx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int num;
+
+	while (num <= 10)
+	{
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+
+	}
+}
