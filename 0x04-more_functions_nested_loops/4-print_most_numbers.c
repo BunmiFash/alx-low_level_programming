19cd4fb8280e8dc8deb763a16e8579b810d6f88e
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-16 07:38:26 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_most_numbers - prints numbers 0 - 9 except 2 and 4
+*Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char i;
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		if (i == '2' || i == '4')
+		{
+			continue;
+		}
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
