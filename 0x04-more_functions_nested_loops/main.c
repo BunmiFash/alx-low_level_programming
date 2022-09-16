e3368d0e00dfdb6958eb5d9ea7d3065a4e4d312e
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-16 06:46:15 +0000

    First commit

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -12,13 +12,19 @@ int main(void)
 {
 	/**
 	*char c = 'w';
-	*/
-	int num = 3;
-/**
+	*
+	int num = '3';
+
 	*printf("%c: %d\n", c, _isupper(c));
-	*/
+	*
 
 	printf("%d: %d\n", num, _isdigit(num));
+	*/
+	int a = 30;
+
+	int b = 10;
+
+	printf("%d", mul(a, b));
 
 	return (0);
 }
