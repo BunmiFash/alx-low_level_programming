5022e7e5854e15fe9e8720ab87678a98b86c60b2
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-19 23:40:32 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes a pointer to an int and updates its value
+* @n: Parameter that is updated.
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
