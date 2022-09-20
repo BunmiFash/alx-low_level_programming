42c5cb62ddcc14995aed381fb130f39cb187b222
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-20 04:31:34 +0000

    Reverse strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string in reverse
+* @s: String to be printed in reverse
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int length, i;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		++s;
+	}
+	for (i = length; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
