d7ea03b269f17495b386ca59aeb426b95de8d664
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-23 03:56:13 +0000

    Rot13 encoded

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a sring using rot13
+* @s: String to be encoded
+* Return: encoded rot13 string
+*/
+
+char *rot13(char *s)
+{
+	int i;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		i = 0;
+
+		while (i <= 52)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+			i++;
+		}
+		s++;
+	}
+	return (Ptr);
+}
