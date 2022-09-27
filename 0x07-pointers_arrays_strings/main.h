105b6e15bb226ff505b1455f9235917079e527c4
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-09-27 18:32:51 +0000

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-_putchar(char c);
+int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
 
@@ -17,7 +17,7 @@ char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
 
-void print_diagsums(Int *a, int size):
+void print_diagsums(int *a, int size):
 
 void set_string(char **s, char *to);
 
