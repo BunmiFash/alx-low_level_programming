1cb9fa9e9a073bd77ba24a921647e22e5a096765
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-10-06 21:35:54 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			return (NULL);
 		}
-		return (new_ptr);
+		else
+		{
+			return (new_ptr);
+		}
 	}
 	if (new_size == old_size)
 	{
