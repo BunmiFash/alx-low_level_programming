352418e84e56f100fe5a54548423114c48c2e01e
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-10-10 22:26:52 +0000

    Betty doc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,31 @@
 #ifndef D0G_H
 #define DOG_H
 
+/**
+* struct dog - Dog data type
+*
+* @name: Dog's name
+*
+* @age: Dog's age
+*
+* @owner: Dog's owner
+*
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name. float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
