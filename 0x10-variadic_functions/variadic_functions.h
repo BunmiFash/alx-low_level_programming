2dcc66002f69c75de50e163036a0d56dd568f5e9
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-10-13 21:43:12 +0000

    Haeder file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef VARIADIC_H_
 #define VARIADIC_H_
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct printer{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 
 int _putchar(char c);
 
