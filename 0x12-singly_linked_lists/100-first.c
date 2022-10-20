e9f5f7eff0df790c2ac476432d1e23eed3de4efc
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-10-20 23:20:54 +0000

    Before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* before_main - function to print character before main
+*
+* Return: void
+*/
+
+void before_main(void) __attribute__((constructor));
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
