989b18e0b30d84e1bfdc59899daf296470f82e27
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2022-10-28 04:39:00 +0000

    Char

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,8 +20,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != 0 || b[i] != 1)
+		if (b[i] != '0' || b[i] != '1')
 			return (0);
+
 		num <<= 1;
 		if (b[i] == '1')
 		{
