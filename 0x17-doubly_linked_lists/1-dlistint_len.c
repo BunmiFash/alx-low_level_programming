2ff03efaec6f165e10ff19c64c5e6ea7726062e4
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-16 23:36:57 +0000

    Number of nodes

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,29 +1,26 @@
 #include "lists.h"
 
 /**
-* dlistint_len - function to print list elements
-* @h: pointer to a node of the list
-* Return: Size of the list
-*/
+ * dlistint_len -  A function that returns the number of nodes of a linked list
+ *
+ * @h: Pointer to the first node
+ *
+ * Return: Number of nodes
+ */
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t count = 0;
 
+	if (h == NULL)
+	{
+		return (count);
+	}
 
-size_t num = 0;
-if (h == NULL)
-return (num);
-
-while (h->prev != NULL)
-{
-h = h->prev;
-}
-
-while (h != NULL)
-{
-h = h->next;
-num++;
-}
-
-return (num);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
