4a7f2ae7bbc8845f95d84a2f80c3719e08be8825
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-03-17 01:13:11 +0000

    Get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Returns the node at the specified index
+ *
+ * @head: Pointer to the first node
+ *
+ * @index: Position of node
+ *
+ * Return: Specified node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = head;
+	size_t list_size = dlistint_len(head);
+
+	if (index > list_size - 1)
+	{
+		return (NULL);
+	}
+
+	while (index > 0)
+	{
+		ptr = ptr->next;
+		index--;
+	}
+	return (ptr);
+}
