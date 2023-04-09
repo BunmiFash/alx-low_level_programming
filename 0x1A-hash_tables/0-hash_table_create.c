bc166e66ab0a18074a64a17de596476a0e01ba40
Blessing Oluwabunmi Fashina <foluwaboonmy@gmail.com>
2023-04-09 20:28:49 +0000

    table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates a Hash Table
+ *
+ * @size: size of the hash table
+ *
+ * Return: Pointer to the table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	unsigned long int i;
+
+	if (!table)
+	{
+		return (NULL);
+	}
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!table->array)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
