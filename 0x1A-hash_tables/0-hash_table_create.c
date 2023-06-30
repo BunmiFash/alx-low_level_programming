28fefa47349f2b143876922a272a80e84894cbfc
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-06-30 22:43:07 +0100

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	unsigned long int i;
 
-	if (!table)
+	if (table == NULL)
 	{
 		return (NULL);
 	}
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (!table->array)
+	if (table->array == NULL)
 	{
 		return (NULL);
 	}
