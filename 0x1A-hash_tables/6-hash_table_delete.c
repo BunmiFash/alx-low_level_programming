a32304771d303e94c547e4fd7c86fff6781ad904
Fashina Blessing Oluwabunmi <foluwaboonmy@gmail.com>
2023-07-01 03:06:17 +0100

    del

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,17 +9,21 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *tmp = NULL;
+	hash_table_t *head = ht;
+	hash_node_t *tmp, *ptr = NULL;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
-		while (ht->array[i] != NULL)
+		while (tmp != NULL)
 		{
-			tmp = ht->array[i];
+			ptr = tmp->next;
 			free(tmp->value);
 			free(tmp->key);
-			ht->array[i] = ht->array[i]->next;
+			free(tmp);
+			tmp = ptr;
 		}
 	}
+	free(head->array);
+	free(head);
 }
